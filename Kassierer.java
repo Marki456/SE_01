bb1c598e4194ce1ab21e6b958a975ba74352b71f
aguentepe <abdullah@guentepe.com>
2018-01-09 17:06:45 +0100

    Formatieren des Kassierers

--- Kassierer.java
+++ Kassierer.java
@@ -1,17 +1,20 @@
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 public class Kassierer {
+
     private SimpleDateFormat sdf;
-                
-    public Kassierer(){
-        	sdf = new SimpleDateFormat("HH");
+
+    public Kassierer() {
+        sdf = new SimpleDateFormat("HH");
     }
-    
-    public double kassieren(Kunde k, double Preis){
-		String uhrzeit = sdf.format(new Date());
-		int Jetzt = Integer.parseInt(uhrzeit);
-		double Rechnung = ((double)(k.getAnkunftsZeit() - Jetzt)) * Preis;
-		k.setHatBezahlt();
-		return Rechnung;
+
+    public double kassieren(Kunde k, double Preis) {
+        String uhrzeit = sdf.format(new Date());
+        int Jetzt = Integer.parseInt(uhrzeit);
+        double Rechnung = ((double) (k.getAnkunftsZeit() - Jetzt)) * Preis;
+        k.setHatBezahlt();
+        return Rechnung;
     }
 }
