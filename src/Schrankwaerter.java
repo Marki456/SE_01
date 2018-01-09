44ae8d34b215e2c46c531d6dcf93ec9c4b2db784
aguentepe <abdullah@guentepe.com>
2018-01-09 18:34:41 +0100

    Schrankwaerter.java neu geschrieben.

--- src/Schrankwaerter.java
+++ src/Schrankwaerter.java
@@ -1,35 +1,16 @@
 
 public class Schrankwaerter {
-
-    boolean Schranke = false;
-    //False = Schranke zu
-
-    public void Schranke_schließen() {
-        Schranke = false;
+    
+    public void einfahren(Manager manager) throws ParkhausIstVollException {
+        if (manager.getStellplaetze() == 0)
+            throw new ParkhausIstVollException();
     }
 
-    public void Schranke_oeffnen() {
-        Schranke = true;
-    }
-
-    public boolean getSchranke() {
-        return Schranke;
-    }
-
-    public void einfahren() throws InterruptedException {
-        Schranke_oeffnen();
-
-        Thread.sleep(3000);
-
-        Schranke_schließen();
-    }
-
-    public void ausfahren(Kunde k) throws InterruptedException {
-        if (k.hatBezahlt()) {
-            einfahren();
-        } else {
-            //Bedingung wenn Kunde nicht bezahlt hat. Kann in diesem Szenario nicht auftreten
-        }
+    public void ausfahren(Kunde k) throws KundeHatNichtBezahltException {
+        if (k.hatBezahlt())
+            k.verlassen();
+        else
+            throw new KundeHatNichtBezahltException();
     }
 
 }
