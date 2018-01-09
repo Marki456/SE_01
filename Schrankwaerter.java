29211ea38a687f44c46199ab4ed9581ea2afe9ea
aguentepe <abdullah@guentepe.com>
2018-01-09 17:13:23 +0100

    Formatieren von Schrankwaerter.java

--- Schrankwaerter.java
+++ Schrankwaerter.java
@@ -1,32 +1,35 @@
 
 public class Schrankwaerter {
- boolean Schranke = false;
- //False = Schranke zu
- public void Schranke_schließen(){
-	 Schranke = false;
- }
- public void Schranke_oeffnen(){
-	 Schranke = true;
- }
- public boolean getSchranke(){
-	 return Schranke;
- }
- 
- public void einfahren() throws InterruptedException{
-	 Schranke_oeffnen();
 
-	 Thread.sleep(3000);
+    boolean Schranke = false;
+    //False = Schranke zu
 
-	 Schranke_schließen();
- }
- public void ausfahren(Kunde k) throws InterruptedException{
-	 if(k.hatBezahlt()){
-		 einfahren();
-	 }
-	 else{
-		 //Bedingung wenn Kunde nicht bezahlt hat. Kann in diesem Szenario nicht auftreten
-	 }
- }
- 
+    public void Schranke_schließen() {
+        Schranke = false;
+    }
+
+    public void Schranke_oeffnen() {
+        Schranke = true;
+    }
+
+    public boolean getSchranke() {
+        return Schranke;
+    }
+
+    public void einfahren() throws InterruptedException {
+        Schranke_oeffnen();
+
+        Thread.sleep(3000);
+
+        Schranke_schließen();
+    }
+
+    public void ausfahren(Kunde k) throws InterruptedException {
+        if (k.hatBezahlt()) {
+            einfahren();
+        } else {
+            //Bedingung wenn Kunde nicht bezahlt hat. Kann in diesem Szenario nicht auftreten
+        }
+    }
+
 }
-
