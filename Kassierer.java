53f0b98e97cd8bb6156e7e284ed12e707d7fc103
aguentepe <abdullah@guentepe.com>
2018-01-09 16:40:03 +0100

    Neuer Konstruktor in Kassierer.java damit das Attribut nur einmal erstellt wird

--- Kassierer.java
+++ Kassierer.java
@@ -1,10 +1,13 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 public class Kassierer {
+    private SimpleDateFormat sdf;
+                
+    public Kassierer(){
+        	sdf = new SimpleDateFormat("HH");
+    }
+    
     public int kassieren(Kunde k, int Preis){
-       
-        
-        	SimpleDateFormat sdf = new SimpleDateFormat("HH");
 		String uhrzeit = sdf.format(new Date());
 		int Jetzt = Integer.parseInt(uhrzeit);
 		int Rechnung = (k.getAnkunftsZeit() - Jetzt)*Preis;
