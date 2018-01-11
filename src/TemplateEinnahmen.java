7d654049c0b324fd8d1cd9d1083855607d872561
aguentepe <abdullah@guentepe.com>
2018-01-11 16:38:05 +0100

    TemplateEinnahmen.java neue werte()-Methode

--- src/TemplateEinnahmen.java
+++ src/TemplateEinnahmen.java
@@ -1,16 +1,35 @@
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public abstract class TemplateEinnahmen {
+
+    SimpleDateFormat sdf;
+
+    public TemplateEinnahmen() {
+        sdf = new SimpleDateFormat("HH");
+    }
+
     public double summe(List<Einnahme> list) {
-        SimpleDateFormat sdf = new SimpleDateFormat("HH");
-	String uhrzeitString = sdf.format(new Date());
+        String uhrzeitString = sdf.format(new Date());
         int uhrzeit = Integer.parseInt(uhrzeitString);
         return list.stream()
                 .filter(einnahme -> uhrzeit - einnahme.getTimestamp() < zeitraum())
                 .mapToDouble(einnahme -> einnahme.getBetrag())
                 .sum();
+    }
+
+    public List<Double> werte(List<Einnahme> list) {
+        String uhrzeitString = sdf.format(new Date());
+        int uhrzeit = Integer.parseInt(uhrzeitString);
+        List<Double> werte = new ArrayList<>();
+        list.stream()
+                .filter(einnahme -> uhrzeit - einnahme.getTimestamp() < zeitraum())
+                .mapToDouble(einnahme -> einnahme.getBetrag())
+                .forEach(d -> werte.add(d));
+        return werte;
     }
 
     public abstract int zeitraum();
