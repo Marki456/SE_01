7a2921efd25ad5c98cbea51f8875feeac50e0915
aguentepe <abdullah@guentepe.com>
2018-01-12 14:43:59 +0100

    Kunden bezahlen jede angefangene Stunde anstatt jede beendete Stunde

--- src/Kassierer.java
+++ src/Kassierer.java
@@ -4,7 +4,7 @@ public class Kassierer {
     public double kassieren(Kunde k, double Preis) {
         if (k.hatBezahlt())
             return 0;
-        double Rechnung = ((double) (k.getAnkunftsZeit() - Zeit.getZeit())) * Preis;
+        double Rechnung = ((double) (k.getAnkunftsZeit() - Zeit.getZeit()) + 1) * Preis;
         k.setHatBezahlt();
         return Rechnung;
     }
